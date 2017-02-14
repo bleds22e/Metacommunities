216644ae23b0796fbf3909d7edb4c8807eee4702
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2017-02-14 17:37:21 -0500

    add another plot to script

--- scripts/generating_plots.R
+++ scripts/generating_plots.R
@@ -1,11 +1,19 @@
 # generating graphs
 # 2/14/2017
 
+# LIBRARIES
+
 library(tidyr)
 library(ggplot2)
 
-# plot of metrics and significance
+# DATA
+
 meta <- read.csv("data/metacom_full.csv", header = TRUE)
+
+# PLOTS
+
+# plot of metrics and significance
+
 meta_df <- meta[,-c(1,9)]
 meta_df <- gather(meta_df, 2:7, key = metric, value = value)
 
@@ -39,3 +47,24 @@ ggplot(meta_df, aes(x = period, y = value)) +
 
 # plot metrics, including significance as color
 
+meta_df2 <- spread(meta_df[,-2], key = significance, value = value)
+
+meta_df2$sig_or_NS <- NA
+for (i in 1:length(meta_df2$metric_type)){
+  if (meta_df2$significance[i] > 0.055 | is.na(meta_df2$significance[i])){
+    meta_df2$sig_or_NS[i] = 'NS'
+  } else {
+    meta_df2$sig_or_NS[i] = 'sig'
+  }
+}
+
+ggplot(meta_df2, aes(x = period, y = metric)) +
+  geom_col(aes(fill = sig_or_NS)) +
+  geom_smooth(method = "loess") +
+  facet_grid(. ~ metric_type, scales = "free") +
+  theme_bw()
+
+# show numbers of metacom categories
+
+ggplot(meta, aes(x = pattern)) +
+  geom_bar()
